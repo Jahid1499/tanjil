c4e7250c0e732315f1432ae6c1cc4791c739444d
jahid <jahid15-1499@diu.edu.bd>
2025-08-04 23:09:55 +0600

    async await

---
+++ main.js
@@ -0,0 +1,17 @@
+const promise1 = Promise.resolve("this is a resolved promise 1");
+const promise2 = new Promise((resolve, _reject) => {
+  setTimeout(() => {
+    resolve("this is a rejected promise 2");
+  }, 5000);
+});
+const promise3 = Promise.resolve("this is a resolved promise 3");
+
+Promise.allSettled([promise1, promise2, promise3])
+  .then((values) => {
+    console.log("All promises resolved:");
+    console.log(values);
+  })
+  .catch((error) => {
+    console.log("At least one promise rejected:");
+    console.log(error);
+  });
